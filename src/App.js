9c16c843715c1740c56491cf2ba5de7192115d7c
Ramya V <ramya@Ramyas-MacBook-Air.local>
2021-12-28 11:11:56 +0530

    working code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
+
+  const [time, setTime] = useState(0);
+  const [timerOn, setTimerOn] = useState(false);
+
+  useEffect(() => {
+    
+    let interval = null;
+
+    if(timerOn){
+      interval = setInterval(()=>{
+        setTime(prevTime => prevTime+10)
+      }, 10)
+    }else{
+      clearInterval(interval)
+    }
+    return ()=>{clearInterval(interval)}
+  }, [timerOn])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="Timers">
+        <h2>Stopwatch</h2>
+        <div id="display">
+          <span>
+            {("0" + Math.floor(time/60000) % 60).slice(-2)}
+          </span>
+          :
+          <span>
+            {("0" + Math.floor(time/1000) % 60).slice(-2)}
+          </span>
+          :
+          <span>
+            {("0" + Math.floor(time/10) % 100).slice(-2)}
+          </span>
+        </div>
+
+        <div id="buttons">
+          {!timerOn && time ===0 &&
+        <button onClick={()=>setTimerOn(true)}>
+          Start
+        </button>}
+
+        {timerOn && 
+        <button onClick={()=>setTimerOn(false)}>
+          Stop
+        </button>}
+
+        {!timerOn && time !== 0 &&
+        <button onClick={()=>setTimerOn(true)}>
+          Resume
+        </button>}
+        {!timerOn && time > 0 &&
+        <button onClick={()=>setTime(0)}>
+          Reset
+        </button>}
+        </div>
+      </div>
   );
 }
 
